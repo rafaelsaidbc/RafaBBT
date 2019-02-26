a1be1a6b259660eded91342178b2eb48d0f6a61d
Rafael Said <rafasaid@gmail.com>
2019-02-26 16:45:17 -0300

    Spinner para selecionar ano de exibição da lista de livros lidos

--- rafa_bbt.py
+++ rafa_bbt.py
@@ -37,6 +37,7 @@ def conexao_firebase():
 conexao_firebase()
 
 
+# função para adicionar livros na lista de livros que quero ler
 def adicionar_livros_lista():
     # obtém a referência da database no Firebase
     referencia = db.reference()
@@ -55,6 +56,7 @@ def adicionar_livros_lista():
     novo_post.set(dicionario_livro)
 
 
+#função da janela de cadastro dos livros que quero ler
 def janela_cadastro_livros_ler():
     janela_livros_ler = Toplevel()
     janela_livros_ler.title('Cadastro de livros na lista de leitura')
@@ -72,6 +74,114 @@ def janela_cadastro_livros_ler():
                                                                                                    sticky=W, pady=4)
 
 
+# função para exibir a lista de livros que quero ler
+def exibir_lista_ler():
+    # pega a referência na database do Firebase
+    referencia = db.reference()
+    # pega os itens que estão catalogados na database
+    livros_ler = referencia.child('livrosLer').get().items()
+    # cria uma janela Toplevel (que irá aparecer após clicar no botão correspondente)
+    janela_livros_ler = Toplevel()
+    # título da janela
+    janela_livros_ler.title('LISTA DE LIVROS QUE DESEJO LER')
+    # cria a variável t do tipo Text para inserir os dados
+    t = Text(janela_livros_ler)
+    # pega cada elemento (key) do Firebase
+    for elemento in livros_ler:
+        # pega a posição [1] do elemento (que se refere aos dados de título e autor, a posição [0] se refere à key do Firebase
+        dados = elemento[1]
+        # para cada chave, valor do dicionário dos dados
+        for chave, valor in dados.items():
+            # se a chave for igual a autor
+            if chave == 'autor':
+                # armazena o nome do autor(es) na variável autor
+                autor = 'Autor(es): ' + valor
+            # se a chave for igual a título
+            if chave == 'titulo':
+                # armazena o título do livro na variável título
+                titulo = 'Título: ' + valor
+                # insere as informações numa lista com a formatação aqui definida
+                t.insert(END, '\n' + '\n' + autor + '\n' + titulo + '\n' + '\n' + '*' * 50)
+    # empacota a variável t para ser exibida
+    t.pack()
+
+
+# função para exibir a lista de livros que li em determinado ano
+def exibir_lista_lidos():
+    # Cria formulario
+    formulario = Toplevel()
+    formulario.title = 'Escolha um ano'
+
+    # Evento On Click do botão
+    def clique():
+        # pega o ano que está no spinner e converte para string
+        ano = spinner.get()
+        # mostra uma mensagem na janela informando o ano selecionado
+        txt = 'Você selecionou: ' + spinner.get()
+        # cria um Label para exibir as informações
+        texto = Label(formulario, text=txt)
+        # localiza o Label na janela
+        texto.grid(row=2, sticky=W, padx=20)
+        # pega a referência na database do Firebase
+        referencia = db.reference()
+        # pega os itens que estão catalogados na database
+        livros_lidos = referencia.child('livrosLidos').child(ano).get().items()
+        # cria uma janela Toplevel (que irá aparecer após clicar no botão correspondente)
+        janela_livros_lidos = Toplevel()
+        # título da janela
+        janela_livros_lidos.title('LISTA DE LIVROS QUE DESEJO LER')
+        # cria a variável t do tipo Text para inserir os dados
+        t = Text(janela_livros_lidos)
+        # pega cada elemento (key) do Firebase
+        for elemento in livros_lidos:
+            # pega a posição [1] do elemento (que se refere aos dados de título e autor, a posição [0] se refere à key do Firebase
+            dados = elemento[1]
+            # para cada chave, valor do dicionário dos dados
+            for chave, valor in dados.items():
+                if chave == 'anoEdicao':
+                    ano_edicao = 'Ano de Edição: ' + valor
+                # se a chave for igual a autor
+                if chave == 'autor':
+                    # armazena o nome do autor(es) na variável autor
+                    autor = 'Autor(es): ' + valor
+                if chave == 'cidadeEditora':
+                    cidade_editora = valor
+                if chave == 'editora':
+                    editora = 'Editora: ' + valor
+                if chave == 'fimLeitura':
+                    fim_leitura = 'Término da leitura: ' + valor
+                if chave == 'id_livro':
+                    id_livro = valor
+                if chave == 'inicioLeitura':
+                    inicio_leitura = 'Início da leitura: ' + valor
+                if chave == 'paginas':
+                    paginas = 'Páginas: ' + valor
+                # se a chave for igual a título
+                if chave == 'titulo':
+                    # armazena o título do livro na variável título
+                    titulo = 'Título: ' + valor
+                    # insere as informações numa lista com a formatação aqui definida
+                    t.insert(END,
+                             '\n' + '\n' + id_livro + '\n' + autor + '\n' + titulo + '\n' + paginas + '\n' + inicio_leitura + '\n' + fim_leitura + '\n' + editora + ', ' + cidade_editora + '\n' + ano_edicao + '\n' + '\n' + '*' * 50)
+        # empacota a variável t para ser exibida
+        t.pack()
+
+    # Cria componentes da janela de seleção do ano para exibir os livros lidos no ano selecionado
+    descricao = Label(formulario, text="Selecione um ano entre 2013 e 2019")
+    # cria um spinner, de ano de 2013 ao ano de 2019
+    spinner = Spinbox(formulario, from_=2013, to=2019)
+    # cria um Button, com o comando clique para exibir a lista de livros no ano selecionado
+    botao = Button(formulario, text="Ok", command=clique)
+    # Alinha componentes na tela
+    descricao.grid(row=0, sticky=W, padx=20)
+    spinner.grid(row=1, sticky=W, padx=20)
+    botao.grid(row=3, sticky=W, padx=20, pady=10)
+
+    # executa a janela de seleção do ano a ser exibido a lista de livros lidos
+    mainloop()
+
+
+#função para inserir livros lidos no Firebase
 def inserir_livros_lidos():
     # obtém a referência da database no Firebase
     referencia = firebase_admin.db.reference()
@@ -146,7 +256,6 @@ def adicionar_livros_lidos():
     btn_adicionar = Button(janela_cadastro_livros, text='Inserir', command=inserir_livros_lidos).grid(row=10, column=1,
                                                                                                       sticky=W, pady=4)
 
-
 FONTE = ("Verdana", 12)
 
 
@@ -201,7 +310,7 @@ class PaginaListaLer(tk.Frame):
         label = tk.Label(self, text='Lista de livros a serem lidos', font=FONTE)
         label.pack(pady=10, padx=10)
 
-        btn_exibir = tk.Button(self, text='Exibir lista de leitura', command=lambda: controller.show_frame(''))
+        btn_exibir = tk.Button(self, text='Exibir lista de leitura', command=exibir_lista_ler)
         btn_exibir.pack()
 
         btn_cadastrar = tk.Button(self, text='Cadastrar livro', command=janela_cadastro_livros_ler)
@@ -219,26 +328,8 @@ class PaginaLidos(tk.Frame):
         btn_cadastrar = tk.Button(self, text='Cadastrar', command=adicionar_livros_lidos)
         btn_cadastrar.pack()
 
-        btn2013 = tk.Button(self, text='2013')
-        btn2013.pack()
-
-        btn2014 = tk.Button(self, text='2014')
-        btn2014.pack()
-
-        btn2015 = tk.Button(self, text='2015')
-        btn2015.pack()
-
-        btn2016 = tk.Button(self, text='2016')
-        btn2016.pack()
-
-        btn2017 = tk.Button(self, text='2017')
-        btn2017.pack()
-
-        btn2018 = tk.Button(self, text='2018')
-        btn2018.pack()
-
-        btn2019 = tk.Button(self, text='2019')
-        btn2019.pack()
+        btn_exibir = tk.Button(self, text='Exibir', command=exibir_lista_lidos)
+        btn_exibir.pack()
 
         btn_voltar = tk.Button(self, text='Voltar', command=lambda: controller.show_frame(PaginaPrincipal))
         btn_voltar.pack()
