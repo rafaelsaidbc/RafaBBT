c45778e6c1b2c689730e590bfeceb595842625a4
Rafael Said <rafasaid@gmail.com>
2019-03-11 16:39:30 -0300

    Inserindo a função para atualizar livros lidos

--- rafa_bbt.py
+++ rafa_bbt.py
@@ -37,6 +37,7 @@ def adicionar_livros_lista():
     novo_post.set(dicionario_livro)
     messagebox.showinfo('Lista de livros a serem lidos', 'Livro inserido com sucesso!')
 
+
 #função da janela de cadastro dos livros que quero ler
 def formulario_cadastro_livros_ler():
     janela_livros_ler = Toplevel()
@@ -142,8 +143,8 @@ def exibir_lista_lidos():
     formulario = Toplevel()
     formulario.title = 'Escolha um ano'
 
-    # Evento On Click do botão
-    def clique():
+    # Evento On Click do botão Ok
+    def exibir_lista():
         # pega o ano que está no spinner e converte para string
         ano = spinner.get()
         # mostra uma mensagem na janela informando o ano selecionado
@@ -196,21 +197,168 @@ def exibir_lista_lidos():
         # empacota a variável t para ser exibida
         t.pack()
 
+    # Evento On Click do botão Atualizar
+    def janela_atualizar_livro_lido():
+        # pega o ano que está no spinner e converte para string
+        ano = spinner.get()
+        # mostra uma mensagem na janela informando o ano selecionado
+        txt = 'Você selecionou: ' + spinner.get()
+        # cria um Label para exibir as informações
+        texto = Label(formulario, text=txt)
+        # localiza o Label na janela
+        texto.grid(row=2, sticky=W, padx=20)
+        # pega a referência na database do Firebase
+        referencia = db.reference()
+        # pega os itens que estão catalogados na database
+        livros_lidos_atualizar = referencia.child('livrosLidos').child(ano).get().items()
+        # cria uma janela Toplevel (que irá aparecer após clicar no botão correspondente)
+        janela_livros_lidos = Toplevel()
+        # título da janela
+        janela_livros_lidos.title('Livros lidos em ' + ano)
+        # cria a variável t do tipo scrolledtext para inserir os dados
+        listbox = Listbox(janela_livros_lidos, selectmod=SINGLE, width=60)
+        # pega cada elemento (key) do Firebase
+        for elemento in livros_lidos_atualizar:
+            # pega a posição [1] do elemento (que se refere aos dados de título e autor, a posição [0] se refere à key do Firebase
+            dados = elemento[1]
+            # para cada chave, valor do dicionário dos dados
+            for chave, valor in dados.items():
+                # se a chave for igual a título
+                if chave == 'titulo':
+                    # armazena o título do livro na variável título
+                    titulo = 'Título: ' + valor
+                    # insere as informações numa lista com a formatação aqui definida
+                    listbox.insert(END, titulo)
+        listbox.pack()
+
+        # função para atualizar o livro selecionado
+        def formulario_atualizar_livro_selecionado():
+            janela_atualizar_livro = Toplevel()
+            janela_atualizar_livro.title('Atualização de livro')
+
+            # função para atualizar a opção selecionada na listbox
+            def atualizar():
+                # obtém a posição do item selecionado na lista
+                posicao = int(listbox.curselection()[0])
+                # cria a variável selecionado com o título do livro selecionado na listbox
+                selecionado_listbox = listbox.get(posicao)
+                selecionado = selecionado_listbox[8:]
+
+                # pega a key do Firebase, que corresponde ao elemento na posição [0]
+                dados_firebase = referencia.child('livrosLidos').child(ano).get().items()
+                for chave, dicionario in dados_firebase:
+                    for item, valor in dicionario.items():
+                        if valor == selecionado:
+                            # cria a variável deletar_referencia com o caminho da key selecionada para ser deletada
+                            key_firebase = chave
+                            pegar_referencia = referencia.child('livrosLidos').child(ano).child(
+                                key_firebase).get().items()
+                            for indice, informacao in pegar_referencia:
+                                if indice == "anoEdicao":
+                                    global autalizar_edicao_ano_lido
+                                    autalizar_edicao_ano_lido = informacao
+                                if indice == 'autor':
+                                    global atualizar_autor_lido
+                                    atualizar_autor_lido = informacao
+                                if indice == 'cidadeEditora':
+                                    global atualizar_cidade_editora_lido
+                                    atualizar_cidade_editora_lido = informacao
+                                if indice == 'editora':
+                                    global atualizar_editora_lido
+                                    atualizar_editora_lido = informacao
+                                if indice == 'fimLeitura':
+                                    global atualizar_fim_leitura_lido
+                                    atualizar_fim_leitura_lido = informacao
+                                if indice == 'id_livro':
+                                    global atualizar_id_livro
+                                    atualizar_id_livro = informacao
+                                if indice == 'inicioLeitura':
+                                    global atualizar_inicio_leitura_lido
+                                    atualizar_inicio_leitura_lido = informacao
+                                if indice == 'paginas':
+                                    global atualizar_paginas_lidos
+                                    atualizar_paginas_lidos = informacao
+                                if indice == 'titulo':
+                                    global atualizar_titulo_lido
+                                    atualizar_titulo_lido = informacao
+
+            atualizar()
+
+            Label(janela_atualizar_livro, text='Ano de leitura').grid(row=0)
+            Label(janela_atualizar_livro, text='id livro').grid(row=1)
+            Label(janela_atualizar_livro, text='Título').grid(row=2)
+            Label(janela_atualizar_livro, text='Autor').grid(row=3)
+            Label(janela_atualizar_livro, text='Páginas').grid(row=4)
+            Label(janela_atualizar_livro, text='Início da leitura').grid(row=5)
+            Label(janela_atualizar_livro, text='Fim da leitura').grid(row=6)
+            Label(janela_atualizar_livro, text='Cidade da editora').grid(row=7)
+            Label(janela_atualizar_livro, text='Editora').grid(row=8)
+            Label(janela_atualizar_livro, text='Ano da edição').grid(row=9)
+
+            ano_lido_entry = StringVar(janela_atualizar_livro, value=ano)
+            ano_leitura_atualizar_livro = Entry(janela_atualizar_livro, textvariable=ano_lido_entry, width=40)
+            id_livro_lido_entry = StringVar(janela_atualizar_livro, value=atualizar_id_livro)
+            id_livro_atualizar_livro = Entry(janela_atualizar_livro, textvariable=id_livro_lido_entry, width=40)
+            titulo_lido_entry = StringVar(janela_atualizar_livro, value=atualizar_titulo_lido)
+            titulo_atualizar_livro = Entry(janela_atualizar_livro, textvariable=titulo_lido_entry, width=40)
+            autor_lido_entry = StringVar(janela_atualizar_livro, value=atualizar_autor_lido)
+            autor_atualizar_livro = Entry(janela_atualizar_livro, textvariable=autor_lido_entry, width=40)
+            paginas_lido_entry = StringVar(janela_atualizar_livro, value=atualizar_paginas_lidos)
+            paginas_atualizar_livro = Entry(janela_atualizar_livro, textvariable=paginas_lido_entry, width=40)
+            inicio_leituro_lido_entry = StringVar(janela_atualizar_livro, value=atualizar_inicio_leitura_lido)
+            inicio_leitura_atualizar_livro = Entry(janela_atualizar_livro, textvariable=inicio_leituro_lido_entry,
+                                                   width=40)
+            fim_leitura_lido_entry = StringVar(janela_atualizar_livro, value=atualizar_fim_leitura_lido)
+            fim_leitura_atualizar_livro = Entry(janela_atualizar_livro, textvariable=fim_leitura_lido_entry, width=40)
+            cidade_editora_lido_entry = StringVar(janela_atualizar_livro, value=atualizar_cidade_editora_lido)
+            cidade_editora_atualizar_livro = Entry(janela_atualizar_livro, textvariable=cidade_editora_lido_entry,
+                                                   width=40)
+            editora_lido_entry = StringVar(janela_atualizar_livro, value=atualizar_editora_lido)
+            editora_atualizar_livro = Entry(janela_atualizar_livro, textvariable=editora_lido_entry, width=40)
+            ano_edicao_lido_entry = StringVar(janela_atualizar_livro, value=autalizar_edicao_ano_lido)
+            ano_edicao_atualizar_livro = Entry(janela_atualizar_livro, textvariable=ano_edicao_lido_entry, width=40)
+            ano_leitura_atualizar_livro.grid(row=0, column=1)
+            id_livro_atualizar_livro.grid(row=1, column=1)
+            titulo_atualizar_livro.grid(row=2, column=1)
+            autor_atualizar_livro.grid(row=3, column=1)
+            paginas_atualizar_livro.grid(row=4, column=1)
+            inicio_leitura_atualizar_livro.grid(row=5, column=1)
+            fim_leitura_atualizar_livro.grid(row=6, column=1)
+            cidade_editora_atualizar_livro.grid(row=7, column=1)
+            editora_atualizar_livro.grid(row=8, column=1)
+            ano_edicao_atualizar_livro.grid(row=9, column=1)
+
+            def inserir_atualizacao_livro_lido():
+                return ""
+
+            btn_inserir_atualizacao = Button(janela_atualizar_livro, text='Inserir', command='')
+            btn_inserir_atualizacao.grid(row=10, column=1)
+
+        btn_atualizar_livro = Button(janela_livros_lidos, text='Atualizar',
+                                     command=formulario_atualizar_livro_selecionado)
+        btn_atualizar_livro.pack()
+
+
+
     # Cria componentes da janela de seleção do ano para exibir os livros lidos no ano selecionado
     descricao = Label(formulario, text="Selecione um ano entre 2013 e 2019")
     # cria um spinner, de ano de 2013 ao ano de 2019
     spinner = Spinbox(formulario, from_=2013, to=2019)
-    # cria um Button, com o comando clique para exibir a lista de livros no ano selecionado
-    botao = Button(formulario, text="Ok", command=clique)
+    # cria um Button, com o comando exibir_lista para exibir a lista de livros no ano selecionado
+    botao = Button(formulario, text="Ok", command=exibir_lista)
+    # button para atualizar um livro lido
+    btn_atualizar = Button(formulario, text='Atualizar', command=janela_atualizar_livro_lido)
     # volta à janela anterior
     btn_voltar = Button(formulario, text='Voltar', command=formulario.destroy)
     # Alinha componentes na tela
     descricao.grid(row=0, sticky=W, padx=20)
     spinner.grid(row=1, sticky=W, padx=20)
     botao.grid(row=3, sticky=W, padx=20, pady=10)
-    btn_voltar.grid(row=3, sticky=W, padx=50)
+    btn_atualizar.grid(row=3, sticky=W, padx=50)
+    btn_voltar.grid(row=3, sticky=W, padx=110)
     # executa a janela de seleção do ano a ser exibido a lista de livros lidos
     mainloop()
+
 
 #função para inserir livros lidos no Firebase
 def inserir_livros_lidos():
@@ -251,7 +399,8 @@ def inserir_livros_lidos():
 
     limpar_campos()
 
-# função para adicionar livros lidos no Firebase
+
+#função para adicionar livros lidos no Firebase
 def formulario_adicionar_livros_lidos():
     janela_cadastro_livros = Toplevel()
     janela_cadastro_livros.title('Cadastro de livros lidos')
